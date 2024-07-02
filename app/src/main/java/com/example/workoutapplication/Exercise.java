ec110b9d57d63aa3c7db3066df20349253eb9d96
Xsylum <ufleu091@uottawa.ca>
2024-07-02 15:37:39 -0400

    Create Exercise.java class

---
+++ app/src/main/java/com/example/workoutapplication/Exercise.java
@@ -0,0 +1,65 @@
+package com.example.workoutapplication;
+
+import java.util.LinkedList;
+
+public class Exercise {
+
+    // TODO: check uniqueness (unique name/description?)
+
+    private String name;
+    private String description;
+    private String thumbnailID; // Passing the storing/loading of images onto the activity
+    // private Workouts[] includedInWorkouts;
+    private LinkedList<String> tags; // maybe switch this to pairs or a Tag class (tag name, reference to Tag)
+
+    public Exercise (String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public String getThumbnailID()
+    {
+        return thumbnailID;
+    }
+
+    public void setThumbnailID(String thumbnailID)
+    {
+        this.thumbnailID = thumbnailID;
+    }
+
+    // Tag Methods
+    public void clearAllTags() {
+        tags.clear();
+    }
+
+    public void removeTag(String tag)
+    {
+        tags.remove(tag);
+    }
+
+    public void addTag(String tag)
+    {
+        tags.add(tag);
+    }
+
+}
