7b2167dafb9d8cce2faa4540082b1c951772def6
Xsylum <ufleu091@uottawa.ca>
2024-07-10 17:08:46 -0400

    Add JSON code to ExerciseManagementActivity

--- app/src/main/java/com/example/workoutapplication/ExerciseManagementActivity.java
+++ app/src/main/java/com/example/workoutapplication/ExerciseManagementActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ExerciseManagementActivity extends AppCompatActivity {
@@ -20,8 +31,53 @@ public class ExerciseManagementActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_management);
 
+        /**
+        try {
+            FileOutputStream outputStream = getApplicationContext().openFileOutput("Exercises", Context.MODE_PRIVATE);
+            Exercise tempExercise1 = new Exercise("Deadlift", "Lifting bar real cool");
+            outputStream.write(tempExercise1);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+         */ //TODO: probably just use an xml file to save via SharedPreferences?
+
         // TEMP DEBUG EXERCISES
         Exercise tempExercise1 = new Exercise("Deadlift", "Lifting bar real cool");
+        tempExercise1.addTag("legs");
+        tempExercise1.addTag("Biceps");
+        tempExercise1.setThumbnailID("001.jpg");
+
+        try
+        {
+            // Creating the JSONObject that will hold this exercise (will need array of these objects!
+            JSONObject jsonObject = new JSONObject();
+
+            // Putting exercise's variable values
+            jsonObject.put("Name", tempExercise1.getName());
+            jsonObject.put("Description", tempExercise1.getDescription());
+            jsonObject.put("ThumbnailID", tempExercise1.getThumbnailID());
+
+            // JSONArray to hold exercise's tags
+            JSONArray array = new JSONArray(tempExercise1.getTags());
+            jsonObject.put("Tags", array);
+
+            // String form of the json for this exercise;
+            String exerciseJsonString = jsonObject.toString();
+            //Log.d("Json", exerciseJsonString);
+
+            //TODO: add constructor to Exercise which uses the below JSONObject (built from a JSON string)
+            //JSONObject newObject = new JSONObject(jsonObject.toString());
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+//        catch (IOException e)
+//        {
+//            e.printStackTrace();
+//        }
+
+
         Exercise tempExercise2 = new Exercise("Rows", "Kind of like a boat!");
         Exercise tempExercise3 = new Exercise("Glute Bridge", "Lift dat butt!");
         Exercise tempExercise4 = new Exercise("Bicep Curls", "Working that iron");
