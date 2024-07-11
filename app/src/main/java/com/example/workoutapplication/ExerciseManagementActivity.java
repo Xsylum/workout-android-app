78fa38d2264c46ef5b6bb7acbc6aef26740f9c46
Xsylum <ufleu091@uottawa.ca>
2024-07-11 15:35:22 -0400

    Remove Exercise.java JSON-packaging from ManagementActivity

--- app/src/main/java/com/example/workoutapplication/ExerciseManagementActivity.java
+++ app/src/main/java/com/example/workoutapplication/ExerciseManagementActivity.java
@@ -41,52 +41,20 @@ public class ExerciseManagementActivity extends AppCompatActivity {
         }
          */ //TODO: probably just use an xml file to save via SharedPreferences?
 
-        // TEMP DEBUG EXERCISES
-        Exercise tempExercise1 = new Exercise("Deadlift", "Lifting bar real cool");
-        tempExercise1.addTag("legs");
-        tempExercise1.addTag("Biceps");
-        tempExercise1.setThumbnailID("001.jpg");
+        JSONArray exerciseList = DEBUG_ExerciseList();
 
-        try
-        {
-            // Creating the JSONObject that will hold this exercise (will need array of these objects!
-            JSONObject jsonObject = new JSONObject();
-
-            // Putting exercise's variable values
-            jsonObject.put("Name", tempExercise1.getName());
-            jsonObject.put("Description", tempExercise1.getDescription());
-            jsonObject.put("ThumbnailID", tempExercise1.getThumbnailID());
-
-            // JSONArray to hold exercise's tags
-            JSONArray array = new JSONArray(tempExercise1.getTags());
-            jsonObject.put("Tags", array);
-
-            // String form of the json for this exercise;
-            String exerciseJsonString = jsonObject.toString();
-            //Log.d("Json", exerciseJsonString);
-
-            //TODO: add constructor to Exercise which uses the below JSONObject (built from a JSON string)
-            //JSONObject newObject = new JSONObject(jsonObject.toString());
+        ArrayList<Exercise> tempDataset = new ArrayList<Exercise>();
+        try {
+            tempDataset.add(Exercise.fromJsonString(exerciseList.get(0).toString()));
+            tempDataset.add(Exercise.fromJsonString(exerciseList.get(1).toString()));
+            tempDataset.add(Exercise.fromJsonString(exerciseList.get(2).toString()));
+            tempDataset.add(Exercise.fromJsonString(exerciseList.get(3).toString()));
         }
         catch (JSONException e)
         {
             e.printStackTrace();
         }
-//        catch (IOException e)
-//        {
-//            e.printStackTrace();
-//        }
 
-
-        Exercise tempExercise2 = new Exercise("Rows", "Kind of like a boat!");
-        Exercise tempExercise3 = new Exercise("Glute Bridge", "Lift dat butt!");
-        Exercise tempExercise4 = new Exercise("Bicep Curls", "Working that iron");
-
-        ArrayList<Exercise> tempDataset = new ArrayList();
-        tempDataset.add(tempExercise1);
-        tempDataset.add(tempExercise2);
-        tempDataset.add(tempExercise3);
-        tempDataset.add(tempExercise4);
 
         RecyclerView recyclerView = findViewById(R.id.rv_exerciseManagerList);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -104,5 +72,34 @@ public class ExerciseManagementActivity extends AppCompatActivity {
                 recyclerView.getAdapter().notifyDataSetChanged();
             }
         });
+    }
+
+    public JSONArray DEBUG_ExerciseList() {
+        // TEMP DEBUG EXERCISES
+        Exercise tempExercise1 = new Exercise("Deadlift", "Lifting bar real cool");
+        tempExercise1.addTag("legs");
+        tempExercise1.addTag("Biceps");
+        tempExercise1.setThumbnailID("001.jpg");
+
+        Exercise tempExercise2 = new Exercise("Rows", "Kind of like a boat!");
+        tempExercise2.addTag("Shoulders");
+        tempExercise2.setThumbnailID("002.jpg");
+
+        Exercise tempExercise3 = new Exercise("Glute Bridge", "Lift dat butt!");
+        tempExercise3.addTag("Glutes");
+        tempExercise3.addTag("legs");
+        tempExercise3.setThumbnailID("003.jpg");
+
+        Exercise tempExercise4 = new Exercise("Bicep Curls", "Working that iron");
+        tempExercise4.addTag("Biceps");
+        tempExercise4.setThumbnailID("004.jpg");
+
+        JSONArray resultArray = new JSONArray();
+        resultArray.put(tempExercise1.toJsonString());
+        resultArray.put(tempExercise2.toJsonString());
+        resultArray.put(tempExercise3.toJsonString());
+        resultArray.put(tempExercise4.toJsonString());
+
+        return resultArray;
     }
 }
