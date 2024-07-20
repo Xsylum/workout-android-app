df03220a12fffadbe00215c5beb4094c6f9e018c
Xsylum <ufleu091@uottawa.ca>
2024-07-20 18:57:49 -0400

    Kotlin Migration: Regiment.java, RegimenDesignActivity.java

---
+++ app/src/main/java/com/example/workoutapplication/Regimen.kt
@@ -0,0 +1,28 @@
+package com.example.workoutapplication
+
+/**
+ * A class which allowing the user to define a set of
+ * exercises, to act as a plan for the user's workout(s)
+ */
+class Regimen (var name: String) {
+    val exerciseList = ArrayList<Exercise>();
+
+    // Modifying Exercise List Methods
+    fun addExercise(e: Exercise) {
+        exerciseList.add(e)
+    }
+
+    fun removeExercise(e: Exercise) {
+        exerciseList.remove(e)
+    }
+
+    fun removeExercise(index: Int): Exercise {
+        return exerciseList.removeAt(index)
+    }
+
+    fun swapExerciseOrder(index1: Int, index2: Int) {
+        val temp = exerciseList[index2]
+        exerciseList[index2] = exerciseList[index1]
+        exerciseList[index1] = temp
+    }
+}
