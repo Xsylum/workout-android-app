71c8494da69759878ef79f865c38d75b9b2f3cdd
Xsylum <ufleu091@uottawa.ca>
2024-07-22 11:25:30 -0400

    Integrate ExerciseManagementActivity display list with DataStore

--- app/src/main/java/com/example/workoutapplication/ExerciseManagementActivity.kt
+++ app/src/main/java/com/example/workoutapplication/ExerciseManagementActivity.kt
@@ -6,23 +6,71 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.json.JSONArray
 
 class ExerciseManagementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise_management)
 
+        // Getting DataStore to read/write an ExerciseList Preference
+        val dataStoreSingleton = DataStoreSingleton.getInstance(this)
+        val dataStoreHelper = DataStoreHelper(this, dataStoreSingleton.dataStore!!)
+
+        val exerciseListJsonString = dataStoreHelper.getStringValue("ExerciseList");
+        val exerciseList = JSONArray(exerciseListJsonString)
+
         // Setting up Activity's list of exercises
-        val tempDataset = ArrayList<Exercise>()
+        val displayList = ArrayList<Exercise>()
+        for (i in 0 ..< exerciseList.length()) {
+            val exercise = Exercise.fromJsonString(exerciseList[i].toString())
+            displayList.add(exercise)
+        }
+
         val recyclerView = findViewById<RecyclerView>(R.id.rv_exerciseManagerList)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = ExerciseManagementAdapter(tempDataset)
+        recyclerView.adapter = ExerciseManagementAdapter(displayList)
 
         // Modify exercise list buttons
         var addExerciseButton = findViewById<View>(R.id.btn_addExercise) as Button
         addExerciseButton.setOnClickListener {
-            tempDataset.add(Exercise("test", "test"))
-            recyclerView.adapter!!.notifyItemInserted(tempDataset.size - 1)
+            val addedExercise = Exercise("test", "test")
+
+            displayList.add(addedExercise)
+            recyclerView.adapter!!.notifyItemInserted(displayList.size - 1)
+
+            exerciseList.put(addedExercise.toJsonString())
+            dataStoreHelper.setStringValue("ExerciseList", exerciseList.toString())
         }
+    }
+
+    private fun DEBUG_ExerciseList(): JSONArray {
+        // TEMP DEBUG EXERCISES
+        val tempExercise1 = Exercise("Deadlift", "Lifting bar real cool").apply {
+            addTag("legs")
+            addTag("Biceps")
+            thumbnailID = "001.jpg"
+        }
+        val tempExercise2 = Exercise("Rows", "Kind of like a boat!").apply {
+            addTag("Shoulders")
+            thumbnailID = "002.jpg"
+        }
+        val tempExercise3 = Exercise("Glute Bridge", "Lift dat butt!").apply {
+            addTag("Glutes")
+            addTag("legs")
+            thumbnailID = "003.jpg"
+        }
+        val tempExercise4 = Exercise("Bicep Curls", "Working that iron").apply {
+            addTag("Biceps")
+            thumbnailID = "004.jpg"
+        }
+        val resultArray = JSONArray().apply {
+            put(tempExercise1.toJsonString())
+            put(tempExercise2.toJsonString())
+            put(tempExercise3.toJsonString())
+            put(tempExercise4.toJsonString())
+        }
+
+        return resultArray
     }
 }
--- app/src/main/java/com/example/workoutapplication/MainActivity.kt
+++ app/src/main/java/com/example/workoutapplication/MainActivity.kt
@@ -15,19 +15,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var dataStoreRx: DataStore<Preferences>
 
         // SEE DataStoreSingleton.java for credits and more information
         val dataStoreSingleton = DataStoreSingleton.getInstance(this)
         val dataStoreHelper = DataStoreHelper(this, dataStoreSingleton.dataStore!!)
-
-        // EXERCISE JSON ARRAY
-        val exerciseList = DEBUG_ExerciseList()
-        val exerciseListString = exerciseList.toString()
-
-        dataStoreHelper.setStringValue("ExerciseList", exerciseListString);
-        Log.d("MainTest", dataStoreHelper.getStringValue("ExerciseList"))
-
 
         nextPageButton = findViewById<View>(R.id.nextPageButton) as Button
         nextPageButton!!.setOnClickListener {
@@ -38,35 +29,5 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         }
-    }
-
-    private fun DEBUG_ExerciseList(): JSONArray {
-        // TEMP DEBUG EXERCISES
-        val tempExercise1 = Exercise("Deadlift", "Lifting bar real cool").apply {
-            addTag("legs")
-            addTag("Biceps")
-            thumbnailID = "001.jpg"
-        }
-        val tempExercise2 = Exercise("Rows", "Kind of like a boat!").apply {
-            addTag("Shoulders")
-            thumbnailID = "002.jpg"
-        }
-        val tempExercise3 = Exercise("Glute Bridge", "Lift dat butt!").apply {
-            addTag("Glutes")
-            addTag("legs")
-            thumbnailID = "003.jpg"
-        }
-        val tempExercise4 = Exercise("Bicep Curls", "Working that iron").apply {
-            addTag("Biceps")
-            thumbnailID = "004.jpg"
-        }
-        val resultArray = JSONArray().apply {
-            put(tempExercise1.toJsonString())
-            put(tempExercise2.toJsonString())
-            put(tempExercise3.toJsonString())
-            put(tempExercise4.toJsonString())
-        }
-
-        return resultArray
     }
 }
