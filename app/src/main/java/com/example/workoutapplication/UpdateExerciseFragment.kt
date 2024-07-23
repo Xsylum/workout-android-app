44a8e26ebb9c59d93517bb434189a00b7e1d3383
Xsylum <ufleu091@uottawa.ca>
2024-07-22 23:21:21 -0400

    Create layout for UpdateExercise dialog

--- app/src/main/java/com/example/workoutapplication/UpdateExerciseFragment.kt
+++ app/src/main/java/com/example/workoutapplication/UpdateExerciseFragment.kt
@@ -3,6 +3,7 @@ package com.example.workoutapplication
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.EditText
 import androidx.fragment.app.DialogFragment
 
 class UpdateExerciseFragment : DialogFragment() {
@@ -10,10 +11,20 @@ class UpdateExerciseFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?) : Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater;
+
+            // Inflating/setting the layout for the dialog
+            // Parent view is null because its going in the dialog layout
+            val layoutView = inflater.inflate(R.layout.update_exercise_layout, null)
+            builder.setView(layoutView)
+
+            val exerciseName: EditText = layoutView.findViewById(R.id.et_exerciseName)
+            val exerciseDesc: EditText = layoutView.findViewById(R.id.et_exerciseDescription)
 
             builder.setMessage("My Dialog")
                 .setPositiveButton("Well Done!") {dialog, id ->
                     // Perform positive button consequences
+                    // TODO: define DialogInterface Listener, it should be able to return EditText.getText()
                 }
                 .setNegativeButton("Cancel") { dialog, id ->
                     // Perform negative button consequences
