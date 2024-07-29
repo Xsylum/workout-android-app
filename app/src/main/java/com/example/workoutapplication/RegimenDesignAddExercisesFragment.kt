0cda25c60128dfc4009da8817be3068f586a31bf
Xsylum <fleuryxander@gmail.com>
2024-07-29 16:18:38 -0400

    Include exercise list swapping in RegimenDesignAddExercisesFragment

--- app/src/main/java/com/example/workoutapplication/RegimenDesignAddExercisesFragment.kt
+++ app/src/main/java/com/example/workoutapplication/RegimenDesignAddExercisesFragment.kt
@@ -9,13 +9,18 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.workoutapplication.dataClasses.Exercise
 
-class RegimenDesignAddExercisesFragment(val userExercises: ArrayList<Exercise>): DialogFragment(),
+class RegimenDesignAddExercisesFragment(private val userExercises: ArrayList<Exercise>): DialogFragment(),
     ExerciseManagementAdapter.ExerciseRecyclerViewListener {
 
-    val regimenAdditions = ArrayList<Exercise>()
+    private val addedExercises = ArrayList<Exercise>()
+
+    private lateinit var userExercisesRV: RecyclerView
+    private lateinit var addedExercisesRV: RecyclerView
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
+            addedExercises.add(Exercise("test", "test"))
+
             val builder = AlertDialog.Builder(it)
             val inflater = requireActivity().layoutInflater
 
@@ -23,12 +28,16 @@ class RegimenDesignAddExercisesFragment(val userExercises: ArrayList<Exercise>):
             val layoutView = inflater.inflate(R.layout.fragment_regimen_design_exercises, null)
             builder.setView(layoutView)
 
-            val userExercisesRV = layoutView
-                .findViewById<RecyclerView>(R.id.rv_regimenAvailableExercises)
+            userExercisesRV = layoutView.findViewById<RecyclerView>(R.id.rv_regimenAvailableExercises)
+            addedExercisesRV = layoutView
+                .findViewById<RecyclerView>(R.id.rv_regimenNewExercises)
 
             //TODO figure out how to add recyclerview to a fragment
             userExercisesRV.layoutManager = LinearLayoutManager(requireContext()) // it????
             userExercisesRV.adapter = ExerciseManagementAdapter(userExercises, this)
+
+            addedExercisesRV.layoutManager = LinearLayoutManager(requireContext())
+            addedExercisesRV.adapter = ExerciseManagementAdapter(addedExercises, this)
 
             builder.setPositiveButton("Confirm") { dialog, id ->
 
@@ -38,9 +47,38 @@ class RegimenDesignAddExercisesFragment(val userExercises: ArrayList<Exercise>):
         } ?: throw IllegalStateException("Activity cannot be null")
     }
 
-    override fun onListItemClick(position: Int) {
-        Log.d("RegimenDesignTest", "Exercise ${userExercises[position].exerciseID} "
-                + "is at position $position")
+    override fun onListItemClick(adapter: ExerciseManagementAdapter, position: Int) {
+        if (userExercisesRV.adapter == adapter) {
+            Log.d("RegimenDesignTest", "Exercise ${userExercises[position].name} "
+                    + "is at position $position")
+            addToNewExercises(position)
+            updateRecyclerViewsAddExercise(position)
+        } else {
+            Log.d("RegimenDesignTest", "Exercise ${addedExercises[position].name} "
+                    + "is at position $position")
+            removeFromNewExercises(position)
+            updateRecyclerViewsRemoveExercise(position)
+        }
+    }
+
+    private fun addToNewExercises(position: Int) {
+        val newExercise = userExercises.removeAt(position)
+        addedExercises.add(newExercise)
+    }
+
+    private fun removeFromNewExercises(position: Int) {
+        val oldExercise = addedExercises.removeAt(position)
+        userExercises.add(oldExercise)
+    }
+
+    private fun updateRecyclerViewsAddExercise(position: Int) {
+        userExercisesRV.adapter!!.notifyItemRemoved(position)
+        addedExercisesRV.adapter!!.notifyItemInserted(addedExercises.size - 1)
+    }
+
+    private fun updateRecyclerViewsRemoveExercise(position: Int) {
+        addedExercisesRV.adapter!!.notifyItemRemoved(position)
+        userExercisesRV.adapter!!.notifyItemInserted(userExercises.size - 1)
     }
 
 }
