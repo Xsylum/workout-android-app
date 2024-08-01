3817272c0eaf46dde837798ca2d3a97d11aaa579
Xsylum <fleuryxander@gmail.com>
2024-08-01 11:09:27 -0400

    Add Basic unit tests for ExerciseMetricValue construction

--- app/src/test/java/com/example/workoutapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/workoutapplication/ExampleUnitTest.kt
@@ -1,7 +1,10 @@
 package com.example.workoutapplication
 
+import com.example.workoutapplication.dataClasses.ExerciseMetricValue
 import org.junit.Assert
 import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertAll
+import java.lang.IllegalArgumentException
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,5 +15,20 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         Assert.assertEquals(4, (2 + 2).toLong())
+    }
+
+    @Test
+    fun addition_notCorrect() {
+        Assert.assertNotEquals(-1, (2 + 2).toLong())
+    }
+
+    @Test
+    fun exerciseMetricValueFormat_tooLow() {
+        Assert.assertThrows(IllegalArgumentException::class.java) { ExerciseMetricValue(-4) }
+    }
+
+    @Test
+    fun exerciseMetricValueFormat_isCorrect() {
+        assertAll( { ExerciseMetricValue(0) } )
     }
 }
