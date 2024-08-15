656e846750b322689b68174368b2ce12868a46cf
Xsylum <fleuryxander@gmail.com>
2024-08-15 16:29:00 -0400

    Design SchedulingActivity's layout

---
+++ app/src/main/java/com/example/workoutapplication/SchedulingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.workoutapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SchedulingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scheduling)
+    }
+}
